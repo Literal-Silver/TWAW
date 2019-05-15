a79259e2f048ed771e428dbce35452836a534434
Literal-Silver <usscag@gmail.com>
2019-05-15 08:24:00 -0500

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React from "react";
-import logo from "./logo.svg";
+import star from "./star.png";
 import "./App.css";
+import Button from "react-bootstrap/Button";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import FormControl from "react-bootstrap/FormControl";
+import Form from "react-bootstrap/Form";
 
 function App() {
   return (
     <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Texas Weight and Wellness</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Food Protocols</Nav.Link>
+            <NavDropdown title="Contact Us" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">
+                Set up an appointment
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Seminars</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Our Team</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="https://texasweightandwellness.com/">
+                Extra Information
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={star} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,8 +50,27 @@ function App() {
           Learn React yeet yeet
         </a>
       </header>
+      <Button variant="success" onClick={() => alert("YEET!")}>
+        Yeet
+      </Button>
+      <ShoppingList name="ed" />
     </div>
   );
+}
+
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
