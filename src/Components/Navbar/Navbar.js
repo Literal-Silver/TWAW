1fc3be13db28ac5f9d4250ad2fb9a1a3643f9713
Literal-Silver <usscag@gmail.com>
2019-05-29 19:18:47 -0500

    updated navbarTWAW

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,8 +3,6 @@ import Button from "react-bootstrap/Button";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import FormControl from "react-bootstrap/FormControl";
-import Form from "react-bootstrap/Form";
 import logo from "../../img/black_logo.png";
 
 class NavbarTWAW extends React.Component {
