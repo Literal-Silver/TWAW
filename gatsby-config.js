573102014fb1b2c3e68676c1cc679c0d9343b7e1
Edmund Miller <edmund.a.miller@protonmail.com>
2019-11-06 00:20:50 -0600

    feat: change site name

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   siteMetadata: {
-    title: 'Yelloecake',
-    siteUrl: 'https://yellowcake.netlify.com'
+    title: 'TWAW',
+    siteUrl: 'https://twaw.netlify.com'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
