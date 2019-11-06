8a127298f86ad71de9a889823a395763df04b1dd
Edmund Miller <edmund.a.miller@protonmail.com>
2019-11-06 09:18:43 -0600

    fix: remove old stuff

--- src/Components/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Footer extends React.Component {
-  render() {
-    return <div className="Footer">This is where the Footer will be.</div>;
-  }
-}
-
-export default Footer;
